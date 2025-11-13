f65d9761cbb12587a6f0f4f24f5155c70a4f8033
JansQ-75 <janina7590@hotmail.com>
2025-11-13 23:22:01 +0200

    Added few comments, and removed unnecessary parameters and imports

--- app/src/main/java/com/example/heartratelimits/MainActivity.kt
+++ app/src/main/java/com/example/heartratelimits/MainActivity.kt
@@ -1,22 +1,18 @@
 package com.example.heartratelimits
 
-import android.annotation.SuppressLint
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -34,8 +30,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.heartratelimits.ui.theme.HeartRateLimitsTheme
@@ -60,29 +54,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HeartRateLimits(modifier: Modifier = Modifier) {
-    var ageInput by remember { mutableStateOf("") }
-    val age = ageInput.toIntOrNull() ?: 0
-    val upper = if (age > 0) (220 - age) * 0.85f else 0
-    val lower = if (age > 0) (200 - age) * 0.65f else 0
-    val df = DecimalFormat("#.##")
-    df.roundingMode = RoundingMode.CEILING
+    var ageInput by remember { mutableStateOf("") } // tilamuuttuja ikää varten
+    val age = ageInput.toIntOrNull() ?: 0                   // muutetaan syötetty ikä integeriksi
+    val upper = if (age > 0) (220 - age) * 0.85f else 0     // sykkeiden yläraja
+    val lower = if (age > 0) (200 - age) * 0.65f else 0     // sykkeiden alaraja
+    val df = DecimalFormat("#.##")                  // formatoidaan luvut kahden desimaalin tarkkuudella
+    df.roundingMode = RoundingMode.CEILING                  // pyöristetään luvut ylöspäin
 
-    Box(modifier = modifier.fillMaxSize())
+    Box(modifier = modifier.fillMaxSize())              // Box -layout
     {
-        Image(
+        Image(                                          // taustakuva
             painter = painterResource(id = R.drawable.unicorn),
             contentDescription = null,
             contentScale = ContentScale.FillBounds,
             modifier = Modifier.fillMaxSize()
         )
-        Column(
+        Column(                                         // Column -layout laskuria varten
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp),
             verticalArrangement = Arrangement.spacedBy(2.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            TextField(
+            TextField(                              // tekstin syöttökenttä
                 value = ageInput,
                 onValueChange = {ageInput = it},
                 label = {Text(text="Age")},
@@ -94,7 +88,7 @@ fun HeartRateLimits(modifier: Modifier = Modifier) {
 
                 )
             Text(
-                text = stringResource(R.string.your_heart_rate_limits_are, df.format(lower), df.format(upper)),
+                text = stringResource(R.string.your_heart_rate_limits_are),
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.White,
@@ -111,7 +105,7 @@ fun HeartRateLimits(modifier: Modifier = Modifier) {
             )
         }
         if (age > 0) {
-            Text(
+            Text(               // Loppukevennys. Näytetään jos käyttäjä on syöttänyt iän
                 text = "Unicorn power: Activated!",
                 fontSize = 26.sp,
                 color = Color.White,
